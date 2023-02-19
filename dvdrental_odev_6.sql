e0d738850301b4d1bedecd484b8e3e4ef0a33f7a
Hatice BAHAR <57835045+haticebahar@users.noreply.github.com>
2023-02-19 02:33:56 +0200

    Add files via upload

---
+++ dvdrental_odev_6.sql
@@ -0,0 +1,13 @@
+/*1.) film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.*/
+
+	/*select * from film where title LIKE '%n' order by length DESC LIMIT 5;*/
+
+/*2.) film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 
+filmi(6,7,8,9,10) sıralayınız.*/
+
+	/*select * from film where title LIKE '%n' order by length  ASC OFFSET 5 LIMIT 5;*/
+
+/*3.) customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla 
+ilk 4 veriyi sıralayınız.*/
+
+	/*select * from customer where store_id=1 order by last_name DESC LIMIT 4;*/
